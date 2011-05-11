d44a3118dea44e74f89da53f15df09989e9259f7
Mark Lanett <mark.lanett@gmail.com>
2011-05-11 08:42:33 -0700

    config isn't right yet

--- test/self_test.rb
+++ test/self_test.rb
@@ -1,12 +1,12 @@
 require File.expand_path( "../helper", __FILE__ )
-require "rack/client"
 
 # Uses minitest
 # @see http://bfts.rubyforge.org/minitest/
 # @see https://github.com/seattlerb/minitest
 
 describe Ymlconfiguration do
-  it "works" do
-    x.must_equal y
+  it "can read a yml" do
+    Ymlconfiguration.init :env => "test"
+    Ymlconfiguration.yml("service").refute_nil
   end
 end
