f988f9ba675477c5d487c680eb03961afe99cf6c
Mark Lanett <mark.lanett@gmail.com>
2011-05-11 08:42:50 -0700

    uglyness documented

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 
-["lib","test"].map { |d| File.expand_path("../../#{d}", __FILE__) }.each { |d| $:.push(d) unless $:.member?(d) }
+["lib","test"].map { |d| File.expand_path("../../#{d}", __FILE__) }.each { |d| $:.push(d) unless $:.member?(d) } # Ugly!
 
 require "ymlconfiguration"
 require "minitest/autorun"
